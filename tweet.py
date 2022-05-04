7260fbf0c34a54679497f5589a2133ad4b95da41
Andrea Moro <34944866+andreamoro-git@users.noreply.github.com>
2022-05-04 10:11:48 +0200

    commented code

--- tweet.py
+++ tweet.py
@@ -1,28 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Posts random tercet from Dante's Comedy on Twitter and Facebook using their API
+requires API credentials saved in separate files (see code towards the end of this file)
 
-This is a temporary script file.
 """
 
 from twython import Twython, TwythonError 
+import facebook
 import numpy as np
-np.random.rand()
-  
-# setting path
 from dantepage import dantePage
 
+# load the comedy's class
 p = dantePage()
+
+# select one line randomly after removing front matter and copyright notices 
 nlines = p.epbounds[100]-p.epbounds[0]
 randline = round(nlines*np.random.rand())+p.epbounds[0]
 
+# find the canto (idx) this tercet belongs to
 for idx,bound in enumerate(p.epbounds):
     if randline<=p.epbounds[idx+1]:
         cantorand = randline - bound
+        
+        # extract the relevant tercet
         startoffset = cantorand % 3
         begin = randline-startoffset
         end = randline-startoffset+3
         
+        # the end of the canto has four lines, not three
+        # this also takes care of extra lines at the end of the cantica
         if idx==33 or idx==66:
             if end>=p.epbounds[idx+1]-3:
                 end = p.epbounds[idx+1]-2
@@ -31,12 +37,16 @@ for idx,bound in enumerate(p.epbounds):
                 end = p.epbounds[idx+1]-1
         while end-begin<3:
             begin = begin-3
+            
+        # final array of 3 or 4 verses
         randomterc = p.lines[begin:end]
         break
 
+# start preparing the string: remove whitespace and join with newlines      
 randomterc = [line.strip() for line in randomterc]
 tercstr = "\n".join(randomterc)
 
+# canto line numbers
 verses = " ("+str(cantorand-startoffset+1) + "-" + str(cantorand-startoffset+1+len(randomterc))+")"
 
 if idx<34:
@@ -49,6 +59,7 @@ else:
     cantica = 'Par'
     canto = idx-66
 
+# final string, including links to commentary
 tweet = (tercstr + "\n\n-- " + p.epnames[idx].strip() + verses
          + "\n\nCommenti: https://dante.princeton.edu/cgi-bin/dante/campuscgi/mpb/GetCantoSection.pl?INP_POEM="
          + cantica + '&INP_SECT=' + str(canto) + "&INP_START=1&INP_LEN=150&LANG=2"
@@ -57,19 +68,21 @@ tweet = (tercstr + "\n\n-- " + p.epnames[idx].strip() + verses
          + "#row"+str(begin-p.epbounds[idx]+1)
          )
 
-with open('path-/creds-twitter') as f:
+# twitter API magic: load credentials and post
+with open('[path]/creds-twitter') as f:
     lines = f.read().splitlines()
 
 twitter = Twython(lines[0], lines[1], lines[2], lines[3])
 twpost = twitter.update_status(status= tweet)
 
-import facebook
-with open('path-/creds-facebook') as f:
+# facebook API enchilada: load credentials and post
+with open('[path]/creds-facebook') as f:
     lines = f.read().splitlines()
 graph = facebook.GraphAPI(access_token= lines[0], version="3.1")
 
 fbpost = graph.put_object(parent_object='me', connection_name='feed', message=tweet)
 
+# print the final output, including API's responses
 print(tweet)
 print('\nfacebook: ', fbpost)
 print('twitter: ', twpost['user']['name']+'/'+str(twpost['id']))
